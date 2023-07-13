2760cdedc2cadc8d421037eb61855bbba94fcec9
Lindwen <34105327+Lindwen@users.noreply.github.com>
2023-07-13 08:24:32 +0200

    reformate

--- commands/admin/clear.js
+++ commands/admin/clear.js
@@ -1,18 +1,18 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('clear')
-		.setDescription('Supprime les messages dans le channel')
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Supprime les messages dans le channel')
         .addIntegerOption(option => option.setName('nombre').setDescription('Nombre de messages à supprimer').setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
-	async execute(interaction) {
-		deleteCount = interaction.options.getInteger('nombre');
+    async execute(interaction) {
+        deleteCount = interaction.options.getInteger('nombre');
         if (!deleteCount || deleteCount < 1 || deleteCount > 100)
             return interaction.reply({ content: "Veuillez indiquer un nombre entre 1 et 100 pour le nombre de messages à supprimer.", ephemeral: true });
         const fetched = await interaction.channel.messages.fetch({ limit: deleteCount });
         interaction.channel.bulkDelete(fetched)
             .catch(error => interaction.reply({ content: `Impossible de supprimer les messages à cause de: ${error}`, ephemeral: true }));
         interaction.reply({ content: `J'ai supprimé **${deleteCount}** messages !`, ephemeral: true });
-	},
+    },
 };
