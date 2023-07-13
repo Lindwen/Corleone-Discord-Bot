0b57ec188b6144c7237361649ecb93a41a48665a
Lindwen <34105327+Lindwen@users.noreply.github.com>
2023-07-13 15:09:15 +0200

    reformate

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -1,31 +1,31 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("avatar")
-        .setDescription("Affiche l'avatar de l'utilisateur")
-        .addUserOption((option) =>
-            option
-                .setName("utilisateur")
-                .setDescription("Utilisateur à afficher")
-                .setRequired(false)
-        ),
-    async execute(interaction) {
-        let intUser = interaction.options.getUser("utilisateur");
+  data: new SlashCommandBuilder()
+    .setName("avatar")
+    .setDescription("Affiche l'avatar de l'utilisateur")
+    .addUserOption((option) =>
+      option
+        .setName("utilisateur")
+        .setDescription("Utilisateur à afficher")
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    let intUser = interaction.options.getUser("utilisateur");
 
-        if (!intUser) {
-            intUser = interaction.user;
-        }
+    if (!intUser) {
+      intUser = interaction.user;
+    }
 
-        const avatarEmbed = new EmbedBuilder()
-            .setColor(0x0099ff)
-            .setAuthor({
-                name: `${intUser.username} ( ${intUser.id} )`,
-                iconURL: intUser.avatarURL(),
-            })
-            .setImage(intUser.avatarURL())
-            .setTimestamp();
+    const avatarEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setAuthor({
+        name: `${intUser.username} ( ${intUser.id} )`,
+        iconURL: intUser.avatarURL(),
+      })
+      .setImage(intUser.avatarURL())
+      .setTimestamp();
 
-        await interaction.reply({ embeds: [avatarEmbed] });
-    },
+    await interaction.reply({ embeds: [avatarEmbed] });
+  },
 };
