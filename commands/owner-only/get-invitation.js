0c57224dc28d8f0cba0e868d5a5c28bd567f7762
Lindwen <34105327+Lindwen@users.noreply.github.com>
2023-07-15 16:46:59 +0200

    Update get-invitation.js

--- commands/owner-only/get-invitation.js
+++ commands/owner-only/get-invitation.js
@@ -8,16 +8,20 @@ module.exports = {
         .addStringOption(option => option.setName('serveur').setDescription('Nom du serveur').setRequired(true)),
     async execute(interaction) {
         if (interaction.user.id === config.owner_id) {
-            const guild = interaction.client.guilds.cache.find(guild => guild.name === interaction.options.getString('serveur'));
+            const guildName = interaction.options.getString('serveur');
+            const guild = interaction.client.guilds.cache.find(guild => guild.name === guildName);
             if (guild) {
-                const invite = await guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').first().createInvite({ maxAge: 0, maxUses: 0 });
-                await interaction.reply({ content: `L'invitation du serveur **${guild.name}** est : ${invite}`, ephemeral: true });
-            }
-            else {
+                const channel = guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT');
+                if (channel) {
+                    const invite = await channel.createInvite({ maxAge: 0, maxUses: 0 });
+                    await interaction.reply({ content: `L'invitation du serveur **${guild.name}** est : ${invite.url}`, ephemeral: true });
+                } else {
+                    await interaction.reply({ content: 'Aucun salon textuel trouvé sur ce serveur', ephemeral: true });
+                }
+            } else {
                 await interaction.reply({ content: 'Je ne suis pas présent sur ce serveur', ephemeral: true });
             }
-        }
-        else {
+        } else {
             await interaction.reply({ content: 'Vous n\'avez pas la permission d\'utiliser cette commande', ephemeral: true });
         }
     },
